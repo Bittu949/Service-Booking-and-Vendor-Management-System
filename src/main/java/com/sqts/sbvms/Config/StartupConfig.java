57c079ff5215974e63db48866868f31fb0a3d82d
BalkrishnaNaik <naikbalkrishna296@gmail.com>
2026-07-02 22:36:58 +0530

    Providing admin credentials via environmental variables

--- src/main/java/com/sqts/sbvms/Config/StartupConfig.java
+++ src/main/java/com/sqts/sbvms/Config/StartupConfig.java
@@ -3,6 +3,7 @@ package com.sqts.sbvms.Config;
 import com.sqts.sbvms.Entity.User;
 import com.sqts.sbvms.Enum.Role;
 import com.sqts.sbvms.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,20 +11,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class StartupConfig {
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     @Bean
     public ApplicationRunner adminInitializer(
             UserRepository userRepository,
-            PasswordEncoder passwordEncoder){
+            PasswordEncoder passwordEncoder) {
 
-        return   args -> {
-                if(userRepository.existsByRole(Role.ADMIN))
-                    return;
+        return args -> {
+
+            if (userRepository.existsByRole(Role.ADMIN)) {
+                return;
+            }
+
             User user = new User();
-            user.setName("Admin");
-            user.setEmail("admin123@gmail.com");
-            user.setPassword(passwordEncoder.encode("admin@123"));
+            user.setName(adminName);
+            user.setEmail(adminEmail);
+            user.setPassword(passwordEncoder.encode(adminPassword));
             user.setRole(Role.ADMIN);
+
             userRepository.save(user);
-            };
+        };
     }
 }
